76ecbc16adb980ba79d058c8b8bee05adbda1c97
shahbaz4783 <shahbaz4783@gmail.com>
2024-02-06 17:56:05 +0530

    how it works section

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -4,6 +4,7 @@ import ListItem from '@/components/layouts/ListItem';
 import SectionTop from '@/components/layouts/SectionTop';
 import { shopByCategory } from '@/data/carousel-data';
 import { howServicesWorks } from '@/data/static-lists_data';
+import Image from 'next/image';
 
 export default function Services() {
 	return (
@@ -18,19 +19,33 @@ export default function Services() {
 				subhead='Explore a Variety of Freelance Services Across Different Categories'
 				carouselItem={shopByCategory}
 			/>
-			<section>
-				<SectionTop heading='How it works' subhead='' />
-				<article className='flex flex-col gap-2'>
-					{howServicesWorks.map((data) => (
-						<ListItem
-							key={data.heading}
-							title={data.heading}
-							subheading={data.subheading}
-						>
-							{data.icon && <data.icon />}
-						</ListItem>
-					))}
-				</article>
+			<section className='mb-16 flex flex-col justify-between md:flex-row'>
+				<aside>
+					<article className='flex flex-col gap-2 md:mt-16'>
+						<SectionTop
+							heading='How it works'
+							subhead='Streamlined Process, Seamless Results'
+						/>
+						{howServicesWorks.map((data) => (
+							<ListItem
+								key={data.heading}
+								title={data.heading}
+								subheading={data.subheading}
+							>
+								{data.icon && <data.icon />}
+							</ListItem>
+						))}
+					</article>
+				</aside>
+				<aside>
+					<Image
+						draggable={false}
+						src={'/project-works.svg'}
+						alt=''
+						width={600}
+						height={600}
+					/>
+				</aside>
 			</section>
 		</main>
 	);
