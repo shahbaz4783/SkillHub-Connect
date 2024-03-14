a9f44b37a925079d4800b0f0163575061d319e23
shahbaz4783 <shahbaz4783@gmail.com>
2024-03-15 00:14:33 +0530

    fix bottom footer ui bug

--- src/components/shared/BottomNavbar.tsx
+++ src/components/shared/BottomNavbar.tsx
@@ -4,14 +4,14 @@ import React from 'react';
 
 const BottomNavbar = () => {
 	return (
-		<nav className='bg-slate-100 backdrop-blur flex justify-center md:hidden sticky bottom-2 w-11/12 m-auto rounded-full border gap-6 '>
+		<nav className='bg-slate-300 p-2 backdrop-blur flex justify-center md:hidden sticky bottom-2 w-11/12 m-auto rounded-full border gap-6 '>
 			{BOTTOM_NAV_ITEMS.map((data, index) => (
 				<Link
 					key={index}
 					href={data.path}
 					className='flex flex-col gap-2 items-center p-2 rounded-md'
 				>
-					{data.icon && <data.icon size={26} />}
+					{data.icon && <data.icon size={28} />}
 				</Link>
 			))}
 		</nav>
