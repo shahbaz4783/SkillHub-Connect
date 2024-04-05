bf2810cadb2c90a89cee074bfd1ac5d0afce839b
shahbaz4783 <shahbaz4783@gmail.com>
2024-04-05 21:57:48 +0530

    update skills form

--- src/components/layouts/settings/profile/UserSkills.tsx
+++ src/components/layouts/settings/profile/UserSkills.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ProfileUpdateCard from '../../../wrapper/ProfileUpdateCard';
+import UpdateSkills from '@/components/forms/UpdateSkills';
 
 const UserSkills = () => {
   return (
     <ProfileUpdateCard title="Skills">
-      <h1>hi</h1>
+      <UpdateSkills />
     </ProfileUpdateCard>
   );
 };
