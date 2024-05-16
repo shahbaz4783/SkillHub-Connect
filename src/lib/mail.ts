a3588dc2a678e8dd229a11df7c6b209c99ad561a
shahbaz4783 <shahbaz4783@gmail.com>
2024-05-17 02:02:12 +0530

    smtp issue fix

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -3,39 +3,38 @@ import nodemailer from 'nodemailer';
 const email = process.env.EMAIL;
 const password = process.env.EMAIL_PASS;
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  host: 'smtp.gmail.com',
-  tls: {
-    ciphers: 'SSLv3',
-  },
-  port: 587,
-  secure: false,
-  auth: {
-    user: email,
-    pass: password,
-  },
-});
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    tls: {
+      ciphers: 'SSLv3',
+    },
+    port: 587,
+    secure: false,
+    auth: {
+      user: email,
+      pass: password,
+    },
+  });
 
+  export const sendVerificationMail = async (
+    token: string,
+    email: string,
+    name: string,
+  ) => {
+    let confirmLink;
 
-export const sendVerificationMail = async (
-  token: string,
-  email: string,
-  name: string,
-) => {
-  let confirmLink;
+    if (process.env.NODE_ENV === 'development') {
+      confirmLink = `http://localhost:3000/new-verification?token=${token}`;
+    } else {
+      confirmLink = `https://skillhub-connect.vercel.app/new-verification?token=${token}`;
+    }
 
-  if (process.env.NODE_ENV === 'development') {
-    confirmLink = `http://localhost:3000/new-verification?token=${token}`;
-  } else {
-    confirmLink = `https://skillhub-connect.vercel.app/new-verification?token=${token}`;
-  }
-
-  await transporter.sendMail({
-    to: email,
-    from: process.env.EMAIL,
-    subject: 'Please confirm your email',
-    html: `
+    await transporter.sendMail({
+      to: email,
+      from: process.env.EMAIL,
+      subject: 'Please confirm your email',
+      html: `
 		<h2>ACTIVATE YOUR ACCOUNT TODAY</h2>
 
 		<p>Dear ${name}</p>
@@ -45,5 +44,15 @@ export const sendVerificationMail = async (
 		<button>Click Here</button>
 		</a>
 		`,
-  });
-};
+    });
+  };
+
+  const sendMail = async (transporter: any, sendVerificationMail: any) => {
+    try {
+      transporter.sendMail(sendVerificationMail);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  sendMail(transporter, sendMail);
