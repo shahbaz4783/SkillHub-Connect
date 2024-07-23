bcb1ed6dec37a92c4a06f9ad746a43e1d616a94b
shahbaz4783 <shahbaz4783@gmail.com>
2024-07-23 23:40:01 +0530

    shadcn tabe

--- src/app/(root)/(protected)/freelancer/orders/page.tsx
+++ src/app/(root)/(protected)/freelancer/orders/page.tsx
@@ -1,35 +1,50 @@
 import SectionHeading from '@/components/shared/SectionHeading';
 import NoDataFound from '@/components/ui/NoDataFound';
-import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 
 const ManageOrdersPage = () => {
   return (
     <div>
       <SectionHeading title="Manage Orders" />
       <div>
-        <table className="w-full border">
-          <thead>
-            <tr className="border bg-slate-100">
-              <th className="border py-4 font-normal text-slate-600">Buyer</th>
-              <th className="border py-4 font-normal text-slate-600">
-                Catalog Id
-              </th>
-              <th className="border py-4 font-normal text-slate-600">Due On</th>
-              <th className="border py-4 font-normal text-slate-600">Total</th>
-              <th className="border py-4 font-normal text-slate-600">Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* <tr className="text-center">
-              <td>shahbaz</td>
-              <td>hhjvrewkjnsvkjeeaqve</td>
-              <td>23 march, 2024</td>
-              <td>$309</td>
-              <td>In Progress</td>
-            </tr> */}
-          </tbody>
-        </table>
-        <NoDataFound message="You have not any orders at the moment" />
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        {/* <NoDataFound message="You have not any orders at the moment" /> */}
       </div>
     </div>
   );
