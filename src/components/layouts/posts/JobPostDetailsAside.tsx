537de9289c49a54d72fc94319efc968e883c9b67
shahbaz4783 <shahbaz4783@gmail.com>
2024-07-29 22:16:07 +0530

    modify options on own detailed jobPost

--- src/components/layouts/posts/JobPostDetailsAside.tsx
+++ src/components/layouts/posts/JobPostDetailsAside.tsx
@@ -1,10 +1,15 @@
+import { DeleteDialogConfirmation } from '@/components/shared/delete-confirmation-dailog';
 import { Button } from '@/components/ui/button';
 import NoDataFound from '@/components/ui/NoDataFound';
 import { getJobDetailsData } from '@/data/posts';
 import { getUserByID } from '@/data/user';
 import { currentUser } from '@/lib/auth';
 import paths from '@/lib/paths';
-import { isAddressFilled, isProfileCompleted } from '@/lib/validation';
+import {
+  isAddressFilled,
+  isOwnedJobPost,
+  isProfileCompleted,
+} from '@/lib/validation';
 import { Heart } from 'lucide-react';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
@@ -22,36 +27,59 @@ const JobPostDetailsAside = async ({ jobId }: PostDetailsAsideProps) => {
 
   if (!jobDetails) return redirect(paths.home());
 
+  const ownedJobPost = await isOwnedJobPost(jobId);
+
   const profileCompleted = await isProfileCompleted(user?.id || '');
   const addressFilled = await isAddressFilled(user?.id || '');
 
   return (
     <aside className="space-y-14 border-l-[1px] px-6 py-8 lg:w-1/5">
-      {profileCompleted && addressFilled ? (
-        <section className="space-y-4">
-          <Link href={`/client/job-post/${jobId}/apply`}>
-            <Button className="w-full">Apply Now</Button>
-          </Link>
-          <form action="">
-            <Button variant={'outline'} className="w-full space-x-2">
-              <Heart size={20} /> <span>Save Job</span>
-            </Button>
-          </form>
-        </section>
-      ) : (
-        <NoDataFound message="Please complete your address and profile information to apply." />
+      {!ownedJobPost && (
+        <>
+          {profileCompleted && addressFilled ? (
+            <section className="space-y-4">
+              <Link href={`/client/job-post/${jobId}/apply`}>
+                <Button className="w-full">Apply Now</Button>
+              </Link>
+              <form action="">
+                <Button variant={'outline'} className="w-full space-x-2">
+                  <Heart size={20} /> <span>Save Job</span>
+                </Button>
+              </form>
+            </section>
+          ) : (
+            <NoDataFound message="Please complete your address and profile information to apply." />
+          )}
+        </>
       )}
 
-      <div className="space-y-2">
-        <p className="text-sm text-slate-700">
-          Required Connects to submit a proposal: {jobDetails.connectCost}
-        </p>
-        {user && (
+      {ownedJobPost && (
+        <div>
+          <ul>
+            <Link href={`/client/job-post/${jobId}/proposals`}>
+              <li>View proposals</li>
+            </Link>
+            <li>Make Private</li>
+            <Link href={`/client/job-post/${jobId}/edit`}>
+              <li>Edit Posting</li>
+            </Link>
+          </ul>
+        </div>
+      )}
+
+      {!ownedJobPost && (
+        <div className="space-y-2">
           <p className="text-sm text-slate-700">
-            Available Connects: {userInfo?.connects}
+            Required Connects to submit a proposal: {jobDetails.connectCost}
           </p>
-        )}
-      </div>
+          {user && (
+            <p className="text-sm text-slate-700">
+              Available Connects: {userInfo?.connects}
+            </p>
+          )}
+        </div>
+      )}
+
       <div>
         <h2 className="text-lg font-semibold">About the client</h2>
         {jobDetails.user.name}
